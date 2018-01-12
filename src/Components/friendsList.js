b36c09cbf95d5e7e22b1e29f8e9ce0d94df6f7e3
Karthik Jagadeesha <kartu8@gmail.com>
2018-01-12 22:28:15 +0530

    Add project struture

---
+++ src/Components/friendsList.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const FriendsList = _ => {};
+
+export { FriendsList };
---
+++ src/Components/searchBar.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const SearchBar = _ => {};
+
+export { SearchBar };
