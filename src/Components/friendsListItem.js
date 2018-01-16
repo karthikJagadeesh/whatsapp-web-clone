ee53a301780d3fceb12347fdfb6c709a4d4c2e0b
Karthik Jagadeesha <kartu8@gmail.com>
2018-01-16 20:50:44 +0530

    Add hover state to list items

--- src/Components/friendsListItem.js
+++ src/Components/friendsListItem.js
@@ -2,19 +2,31 @@ import React, { Component } from "react";
 import { Div, Label, Img } from "glamorous";
 
 const FriendsListItem = props => {
+  const labelNameStyle = {
+    ":hover": {
+      cursor: "pointer"
+    }
+  };
+
+  const timestampStyle = Object.assign({}, labelNameStyle);
+
   const wrapperStyle = {
     padding: "25px 10px",
     borderBottom: "1px solid rgba(0, 0, 0, 0.05)",
     background: "#fff",
     display: "grid",
-    gridTemplateColumns: "25% 50% 25%"
-  }
+    gridTemplateColumns: "25% 50% 25%",
+    ":hover": {
+      background: "#F4F5F5",
+      cursor: "pointer"
+    }
+  };
 
   return (
     <Div css={wrapperStyle}>
-      <Img alt="image"></Img>
-      <Label>{props.name}</Label>
-      <Label>timestamp</Label>
+      <Img alt="image" />
+      <Label css={labelNameStyle}>{props.name}</Label>
+      <Label css={timestampStyle}>timestamp</Label>
     </Div>
   );
 };
