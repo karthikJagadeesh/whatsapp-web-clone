a513f90168eb4ad0d18c0afcf0d847dd0532079a
Karthik Jagadeesh <kartu8@gmail.com>
2018-01-17 16:41:18 +0530

    Add chatbox header components with icons

---
+++ src/Components/search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Div } from "glamorous";
+import MdSearch from "react-icons/lib/md/search";
+
+const Search = _ => {
+  return (
+    <Div>
+      <MdSearch size={25} color="#666" />
+    </Div>
+  );
+};
+
+export { Search };
