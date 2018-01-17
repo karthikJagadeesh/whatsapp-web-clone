bb40e58307f13a87929a4c781fdf3aac3a39d81d
Karthik Jagadeesh <kartu8@gmail.com>
2018-01-17 13:29:10 +0530

    Add profile header components and icons

---
+++ src/Components/profilePicture.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Div, Img } from "glamorous";
+
+const ProfilePicture = _ => {
+  return (
+    <Div>
+      <Img alt="Profile Pic" c />
+    </Div>
+  );
+};
+
+export { ProfilePicture };
