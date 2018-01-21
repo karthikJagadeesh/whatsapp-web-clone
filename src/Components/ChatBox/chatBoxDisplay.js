9b2b680c9dfdcfe9c9433d88042117f8f25f3abb
Karthik Jagadeesh <kartu8@gmail.com>
2018-01-21 13:18:45 +0530

    messages stack up in chat window

--- src/Components/ChatBox/chatBoxDisplay.js
+++ src/Components/ChatBox/chatBoxDisplay.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { Div } from "glamorous";
 
-const ChatBoxDisplay = ({ message }) => {
+const ChatBoxDisplay = ({ messages }) => {
   const wrapperStyle = {
     background: "#E4DED3",
     height: "100%",
-    overflowY: "scroll"
+    overflowY: "scroll",
+    display: "flex",
+    flexDirection: "column-reverse",
+    paddingBottom: "50px"
   };
+  const messagesListWrapperStyle = {
+    padding: "2px 70px",
+    display: "grid",
+    templateGridColumns: "1fr"
+  };
+  const messageStyle = {
+    maxWidth: "70%",
+    justifySelf: "end",
+    wordWrap: "break-word",
+    overflow: "hidden",
+    border: "1px solid rgba(0, 0, 0, 0.1)",
+    boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.1)",
+    fontSize: "0.9em",
+    borderRadius: "8px",
+    color: "rgb(38, 38, 38)",
+    padding: "8px 6px",
+    background: "rgb(220, 248, 198)"
+  };
+  const messagesList = messages.map(msg => (
+    <Div css={messagesListWrapperStyle}>
+      <Div css={messageStyle}>{msg}</Div>
+    </Div>
+  ));
 
-  return <Div css={wrapperStyle}>{message}</Div>;
+  return <Div css={wrapperStyle}>{messagesList}</Div>;
 };
 
 export { ChatBoxDisplay };
