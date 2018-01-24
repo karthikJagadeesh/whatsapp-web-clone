59194daee1388c8df172a4a0b408d821d08c5d49
Karthik Jagadeesh <kartu8@gmail.com>
2018-01-24 18:46:44 +0530

    Add search feature to the sidebar context box

--- src/Components/ContextBox/searchResults.js
+++ src/Components/ContextBox/searchResults.js
@@ -1,20 +1,60 @@
 import React from "react";
-import { Div } from "glamorous";
+import ReactDOMServer from "react-dom/server";
+import { Div, P, Span } from "glamorous";
 
 const SearchResults = ({ name, searchValue, messagesLog }) => {
-  const wrapperStyle = {
+  const wrapperStyleInitialStyle = {
     marginTop: "50px",
     textAlign: "center",
     color: "rgba(0, 0, 0, 0.4)",
     fontSize: "0.9em"
   };
   const initialDisplay = (
-    <Div css={wrapperStyle}>Search for messages with {name}</Div>
+    <Div css={wrapperStyleInitialStyle}>Search for messages with {name}</Div>
   );
+  const wrapperResultsStyle = {
+    padding: "20px",
+    textAlign: "center"
+  };
+  const resultsStyle = {
+    borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
+    paddingBottom: "1em",
+    color: "rgba(0, 0, 0, 0.6)",
+    fontSize: "0.9em"
+  };
+  const highlightValue = {
+    color: "#339686",
+    fontSize: "1.1em",
+    fontWeight: "700"
+  };
+
   const resultsDisplay = (
-    <Div>
-      <Div>Result1</Div>
-      <Div>Result2</Div>
+    <Div css={wrapperResultsStyle}>
+      {messagesLog
+        .filter(({ text }) =>
+          text.toLowerCase().includes(searchValue.toLowerCase().trim())
+        )
+        .map(({ text }) => {
+          const afterSplit = text
+            .toLowerCase()
+            .split(searchValue.toLowerCase());
+          const highlightedResult = [];
+          afterSplit.forEach((text, index) => {
+            if (index % 2 === 0 && index) {
+              highlightedResult.push(
+                <Span css={highlightValue}>{searchValue}</Span>
+              );
+            }
+            if (index === 1) {
+              highlightedResult.push(
+                <Span css={highlightValue}>{searchValue}</Span>
+              );
+            }
+            highlightedResult.push(text);
+          });
+
+          return <Div css={resultsStyle}>{highlightedResult}</Div>;
+        })}
     </Div>
   );
 
