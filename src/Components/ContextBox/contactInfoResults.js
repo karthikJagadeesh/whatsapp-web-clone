7af30b3eb88e7cfc30467b14eb9b6b8e660f7b62
Karthik Jagadeesha <kartu8@gmail.com>
2018-01-26 14:51:16 +0530

    Add media, mute, starred messages cards to contactInfo Box

--- src/Components/ContextBox/contactInfoResults.js
+++ src/Components/ContextBox/contactInfoResults.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Div, Img, Label } from "glamorous";
-// import MdInsertEmoticon from "react-icons/lib/md/insert-emoticon";
+import { Div, Img, Label, Span } from "glamorous";
+import FaAngleRight from "react-icons/lib/fa/angle-right";
+import Checkbox from "rc-checkbox";
 
 const ProfilePictureCard = ({ picturePath, name }) => {
   const wrapperStyle = {
@@ -8,7 +9,8 @@ const ProfilePictureCard = ({ picturePath, name }) => {
     textAlign: "center",
     background: "#FFF",
     display: "grid",
-    gridTemplateRows: "8fr 2fr"
+    gridTemplateRows: "8fr 2fr",
+    boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.1)"
   };
 
   const Picture = ({ picturePath }) => {
@@ -21,7 +23,10 @@ const ProfilePictureCard = ({ picturePath, name }) => {
       alignSelf: "center",
       width: "50%",
       height: "80%",
-      borderRadius: "50%"
+      borderRadius: "50%",
+      ":hover": {
+        cursor: "pointer"
+      }
     };
     return (
       <Div css={wrapperStyle}>
@@ -56,17 +61,135 @@ const ProfilePictureCard = ({ picturePath, name }) => {
   );
 };
 
+const MediaLinksDocs = _ => {
+  const wrapperStyle = {
+    height: "180px",
+    background: "#FFF",
+    display: "grid",
+    gridTemplateRows: "3fr 7fr",
+    marginTop: "1em",
+    boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.1)"
+  };
+
+  const Title = _ => {
+    const wrapperStyle = {
+      display: "grid",
+      width: "100%",
+      gridTemplateColumns: "9fr 1fr",
+      ":hover": {
+        cursor: "pointer"
+      }
+    };
+    const wrapperLabelStyle = {
+      alignSelf: "center",
+      justifySelf: "center",
+      width: "80%",
+      color: "#128C7E",
+      fontSize: "0.9em",
+      fontWeight: "100"
+    };
+    const wrapperIconStyle = {
+      alignSelf: "center"
+    };
+
+    return (
+      <Div css={wrapperStyle}>
+        <Div css={wrapperLabelStyle}>
+          <Span>Media, Links and Docs</Span>
+        </Div>
+        <Div css={wrapperIconStyle}>
+          <FaAngleRight size={25} color="#666" />
+        </Div>
+      </Div>
+    );
+  };
+
+  return (
+    <Div css={wrapperStyle}>
+      <Title />
+      {/* <MediaLinks /> */}
+    </Div>
+  );
+};
+
+const MuteAndStarred = _ => {
+  const wrapperStyle = {
+    height: "120px",
+    background: "#FFF",
+    display: "grid",
+    gridTemplateRows: "5fr 5fr",
+    marginTop: "1em",
+    boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.1)"
+  };
+
+  const Mute = _ => {
+    const wrapperStyle = {
+      display: "grid",
+      gridTemplateColumns: "8fr 2fr",
+      width: "100%",
+      ":hover": {
+        cursor: "pointer"
+      },
+      borderBottom: "1px solid rgba(0, 0, 0, 0.06)"
+    };
+    return (
+      <Div css={wrapperStyle}>
+        <Span
+          css={{ alignSelf: "center", width: "80%", justifySelf: "center" }}
+        >
+          Mute
+        </Span>
+        <Checkbox style={{ alignSelf: "center", justifySelf: "center" }} />
+      </Div>
+    );
+  };
+
+  const Starred = _ => {
+    const wrapperStyle = {
+      display: "grid",
+      gridTemplateColumns: "8fr 2fr",
+      width: "100%",
+      ":hover": {
+        cursor: "pointer"
+      }
+    };
+    return (
+      <Div css={wrapperStyle}>
+        <Span
+          css={{ alignSelf: "center", width: "80%", justifySelf: "center" }}
+        >
+          Starred Messages
+        </Span>
+        <FaAngleRight
+          style={{ alignSelf: "center", justifySelf: "center" }}
+          size={25}
+          color="#666"
+        />
+      </Div>
+    );
+  };
+
+  return (
+    <Div css={wrapperStyle}>
+      <Mute />
+      <Starred />
+    </Div>
+  );
+};
+
 const ContactInfoResults = ({ picturePath, name }) => {
   const wrapperStyle = {
     height: "100%",
     overflowY: "scroll",
-    background: "teal"
+    background: "#F7F7F7"
   };
   const wrapperProfilePictureCardStyle = {};
   return (
     <Div css={wrapperStyle}>
       <Div css={wrapperProfilePictureCardStyle}>
         <ProfilePictureCard picturePath={picturePath} name={name} />
+        <MediaLinksDocs />
+        <MuteAndStarred />
       </Div>
     </Div>
   );
