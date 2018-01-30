88e99657a4ae58db4288c01020065dc038ef6fdf
Karthik Jagadeesha <kartu8@gmail.com>
2018-01-30 22:44:53 +0530

    Update and build

--- src/network.js
+++ src/network.js
@@ -1,10 +1,10 @@
-// export const profileDataUrl =
-//   "https://my-json-server.typicode.com/karthikJagadeesh/fake-chat-api/profile";
-// export const friendDataUrl =
-//   "https://my-json-server.typicode.com/karthikJagadeesh/fake-chat-api/friends/";
+export const profileDataUrl =
+  "https://my-json-server.typicode.com/karthikJagadeesh/fake-chat-api/profile";
+export const friendDataUrl =
+  "https://my-json-server.typicode.com/karthikJagadeesh/fake-chat-api/friends/";
 
-export const profileDataUrl = "http://localhost:7070/profile";
-export const friendDataUrl = "http://localhost:7070/friends/";
+// export const profileDataUrl = "http://localhost:7070/profile";
+// export const friendDataUrl = "http://localhost:7070/friends/";
 
 export async function fetchData(url) {
   const response = await fetch(url);
